fecc9357c578b3a2bcfcc2311ce2ff0702cba6b9
Rogerio Ushiro <rogerio.ushiro@gmail.com>
2023-12-21 10:45:40 -0300

    feat: creating new decks

--- mazza/src/app/new-deck-form/new-deck-form.component.ts
+++ mazza/src/app/new-deck-form/new-deck-form.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
-import { DeckFormComponent } from '../deck-form/deck-form.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { DeckService } from '../services/deck.service';
 
 @Component({
   selector: 'app-new-deck-form',
   standalone: true,
-  imports: [DeckFormComponent],
+  imports: [CommonModule,
+    ReactiveFormsModule],
   templateUrl: './new-deck-form.component.html',
   styleUrl: './new-deck-form.component.scss'
 })
 
 export class NewDeckFormComponent {
+  newDeckForm!: FormGroup;
+  myDecks: any[] = [];
 
+  constructor(private fb: FormBuilder, private deckService: DeckService) { }
+
+  ngOnInit(): void {
+    this.newDeckForm = this.fb.group({
+      name: ['', Validators.required]
+    });
+
+    this.deckService.createDeck("Cards d'água");
+    this.deckService.createDeck("Cards de fogo");
+    this.deckService.createDeck("Cards de 1");
+    this.deckService.createDeck("Cards de 2");
+    this.deckService.createDeck("Cards de 3");
+    this.deckService.createDeck("Cards de 4");
+    this.deckService.createDeck("Cards de 5");
+    this.myDecks = this.deckService.getAllDecks();
+  }
+
+  onSubmit() {
+    if (this.newDeckForm?.valid) {
+      this.deckService.createDeck(this.newDeckForm.value.name);
+      this.myDecks = this.deckService.getAllDecks();
+      this.newDeckForm.reset();
+      // console.log(this.deckService.getAllDecks());
+    }
+  }
 }
