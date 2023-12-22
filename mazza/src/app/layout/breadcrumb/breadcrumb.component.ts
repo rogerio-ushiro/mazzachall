9bd28fb4aa965bd94a0d739f426550378f314f1f
Rogerio Ushiro <rogerio.ushiro@gmail.com>
2023-12-22 15:49:54 -0300

    feat: dynamic breadcrumb

--- mazza/src/app/layout/breadcrumb/breadcrumb.component.ts
+++ mazza/src/app/layout/breadcrumb/breadcrumb.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule, UrlSegment } from '@angular/router';
+import { DeckService } from '../../../data/deck.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-breadcrumb',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.scss'
 })
 export class BreadcrumbComponent {
+
+  selectedCard: string | undefined;
+  currentDeck: any;
+  isEditPage: boolean = false;
+
+  constructor(private deckService: DeckService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+
+    this.route.url.subscribe(e => {
+      this.isEditPage = e[0].path == "deck-edit"
+    })
+
+
+    this.route.params.subscribe(params => {
+      this.currentDeck = this.deckService.setCurrentDeck(params['id']);
+    });
+  }
 
 }
